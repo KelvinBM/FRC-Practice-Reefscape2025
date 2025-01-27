8f7f1c20a05ae6d234a63b76b42a0f9111d2e612
KelvinBM <kelvinmejia182@gmail.com>
2025-01-26 20:22:36 -0500

    remove unused imports relating to SmartDashboard and Limelight from Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,14 +7,9 @@ package frc.robot;
 import com.ctre.phoenix6.Utils;
 
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.Limelight;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
